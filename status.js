56e672f55eae7560a415ff44efd506da6a05c14f
tschanadam <tschanadam@gmail.com>
2021-11-20 23:35:42 -0500

    Create status.js

---
+++ status.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const token = process.env.TOKEN;
+client.on('ready' () => {
+   console.log('Ready to go')
+  
+  var fs = require('fs');
+fs.readFile('statusinfo.txt', function(err, data) {
+    if(err) throw err;
+    var statusArray = data.toString().split("\n");
+
+    setInterval(() => {
+      client.user.setStatus(process.env.PRESENCE);
+      const random = statusArray[Math.floor(Math.random() * statusArray.length)].split(', ')
+      const status = random[0];
+      const mode = random[1];
+      client.user.setActivity(status, { type: mode })
+
+    }, 3000) //your time of changing status in miliseconds for example 1 second = 1000 ms
+});
+
